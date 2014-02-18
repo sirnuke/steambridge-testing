aebf77249fd23458a11b2bc1e8b8ae96321b9fca
Bryan DeGrendel <bryan@degrendel.com>
2014-02-17 19:55:53 -0500

    Implemented a very simple test Bridge DLL.
    1. Removed a few old files.

---
+++ visualstudio/bridge/bridge.cpp
@@ -0,0 +1,10 @@
+// bridge.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "bridge.h"
+
+void *get_real_object()
+{
+  return NULL;
+}
--- visualstudio/bridge/impl.cpp
+++
@@ -1,7 +0,0 @@
-#include "api.h"
-
-void *get_real_object()
-{
-  return NULL;
-}
-
