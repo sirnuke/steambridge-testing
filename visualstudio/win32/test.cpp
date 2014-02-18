b530d8c39ff9f5f592fb6a39a3b8e2ff1e097358
Bryan DeGrendel <bryan@degrendel.com>
2014-02-17 22:17:50 -0500

    Various fixes to make everything work!
    1. It all (!) works correctly (!!)

--- visualstudio/win32/test.cpp
+++ visualstudio/win32/test.cpp
@@ -11,12 +11,30 @@ Test::Test() : parent(NULL)
 }
 
 
+  
 int Test::function_one(int param_one, bool param_two, const char *param_three)
 {
   int result = 0;
-  fprintf(stderr, "(this=0x%p,parent=0x%p,%i,%i,\"%s\")\n", this, parent, param_one, param_two, param_three);
+  //const char *msg = "eax==%p\n";
+  printf("(this=0x%p,parent=0x%p,%i,%i,\"%s\",%p)\n", this, parent, param_one, param_two, param_three, param_three);
+  /*  For reference, here's how to call printf
   __asm
   {
+    mov eax, this
+    push eax
+    //lea eax, msg
+    mov eax, msg
+    push eax
+    call dword ptr[printf]
+    pop eax
+    pop ecx
+  }
+  printf("Done with first assembler block\n");*/
+  
+  __asm
+  {
+    // Push this onto the stack
+    //push ecx
     // Push the parameters onto the stack
     mov eax, param_three
     push eax
@@ -25,10 +43,19 @@ int Test::function_one(int param_one, bool param_two, const char *param_three)
     push eax
     mov eax, param_one
     push eax
-    // Push this onto the stack
-    push ecx
-    // Get the vtable ecx=this, parent
-    mov eax, [ecx].parent
+    // Get the parent
+    mov eax, [this]
+    mov eax, [eax].parent
+    // Push new this onto the stack
+    push eax
+
+    //mov eax, msg
+    //push eax
+    //call dword ptr printf
+    //pop eax // msg
+    //pop eax // this.parent
+    // Get the vtable - pointer should be in the first 32-bits of the class
+    mov eax, [eax]
     // Lookup the first pointer in the vtable
     mov eax, [eax + 0]
     // Call that memory location
@@ -36,9 +63,15 @@ int Test::function_one(int param_one, bool param_two, const char *param_three)
     // Move the returned value into the result
     mov result, eax
     // Cleanup the stack
-    pop ecx
+    // Restore this
+    //pop ecx
+    // pop this.parent
     pop eax
+    // pop param_one
     pop eax
+    // pop param_two
+    pop eax
+    // pop param_three
     pop eax
   }
   return result;
@@ -47,6 +80,7 @@ int Test::function_one(int param_one, bool param_two, const char *param_three)
 bool Test::function_two(double param_one, void *param_two, float param_three)
 {
   bool result = false;
+  printf("(this=0x%p,parent=0x%p,%f/%i&%i,0x%p,%f)\n", this, parent, param_one, param_one, param_two, param_three, param_three);
   __asm
   {
     // Are float values pushed the same?
@@ -55,15 +89,19 @@ bool Test::function_two(double param_one, void *param_two, float param_three)
     push eax
     mov eax, param_two
     push eax
+    // Push both halfs of param_one
     lea edx, param_one
     mov eax, [edx+4]
     push eax
     mov eax, [edx]
     push eax
+    // Get parent (new this)
+    mov eax, [this]
+    mov eax, [eax].parent
     // Push this onto the stack
-    push ecx
-    // Get the vtable ecx=this, parent
-    mov eax, [ecx].parent
+    push eax
+    // Get the vtable (pointer at this)
+    mov eax, [eax]
     // Lookup the first pointer in the vtable
     mov eax, [eax+4]
     // Call that memory location
@@ -71,12 +109,15 @@ bool Test::function_two(double param_one, void *param_two, float param_three)
     // Move the returned value into the result
     mov result, al
     // Cleanup the stack
-    pop ecx
+    // pop this->parent
+    pop eax
+    // pop param_one (in two parts)
     pop eax
     pop eax
+    // pop param_two
     pop eax
+    // pop param_three
     pop eax
   }
   return result;
 }
-
