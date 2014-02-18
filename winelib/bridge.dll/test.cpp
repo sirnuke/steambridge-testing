6276d4976a7fe55c6f515751519bd392e91080ad
Bryan DeGrendel <bryan@degrendel.com>
2014-02-18 14:10:37 -0500

    Implemented all test functions in Winelib.
    1. Also set the debug channel to just steambridge.

--- winelib/bridge.dll/test.cpp
+++ winelib/bridge.dll/test.cpp
@@ -5,7 +5,7 @@
 
 #include "test.h"
 
-WINE_DEFAULT_DEBUG_CHANNEL(steambridget);
+WINE_DEFAULT_DEBUG_CHANNEL(steambridge);
 
 static Test t;
 
@@ -32,5 +32,23 @@ bool Test::function_two(double param_one, void *param_two, float param_three)
 {
   WINE_TRACE("(this=%p,%f|%i&%i,%p,%f)\n", this, param_one, param_one, param_two, param_three);
   return true;
+}
+
+double Test::function_three(unsigned short param_one)
+{
+  WINE_TRACE("(this=%p,%hu)\n", this, param_one);
+  return 2.12301424E32;
+}
+
+uint64_t Test::function_four()
+{
+  WINE_TRACE("(this=%p)\n", this);
+  return 4294967301;
+}
+
+float Test::function_five(int64_t param_one)
+{
+  WINE_TRACE("(this=%p,%lli)\n", this, param_one);
+  return -1.2849f;
 }
 
--- winelib/bridge.dll/test.h
+++ winelib/bridge.dll/test.h
@@ -3,6 +3,8 @@
 #ifndef  ___TEST_H___
 #define  ___TEST_H___
 
+#include <stdint.h>
+
 extern "C"
 {
 void *get_real_object();
@@ -14,6 +16,9 @@ class Test
     Test();
     virtual int function_one(int param_one, bool param_two, const char *param_three); 
     virtual bool function_two(double param_one, void *param_two, float param_three);
+    virtual double function_three(unsigned short param_one);
+    virtual uint64_t function_four();
+    virtual float function_five(int64_t param_one);
 };
 
 
